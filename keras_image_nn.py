379449f959587752874203b931b41b57c379ea03
Andreas Haselsteiner <a.haselsteiner@uni-bremen.de>
2023-09-13 14:42:47 +0200

    Add keras tensor flow model and change nn structure of pytorch.

---
+++ keras_image_nn.py
@@ -0,0 +1,108 @@
+# This code builds the same neural network as in torch_image_nn.py
+# but uses the Keras package as an abstraction layer.
+# This medium post helped me write the code: https://medium.com/p/555007a50c2e
+
+import numpy as np
+import matplotlib.pyplot as plt
+from time import perf_counter
+
+import tensorflow as tf
+from keras.models import load_model
+
+do_train = True
+n_epochs_to_train = 3
+
+model_fname = "keras_model.h5"
+
+
+if __name__ == "__main__":
+    mnist = tf.keras.datasets.mnist
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train, x_test = x_train / 255.0, x_test / 255.0
+    x_train = np.expand_dims(x_train, axis=-1)
+    x_test = np.expand_dims(x_test, axis=-1)
+
+    # Training
+    if do_train:
+        fig = plt.figure(figsize=(5, 4))
+        for idx in np.arange(10):
+            ax = fig.add_subplot(2, 5, idx + 1, xticks=[], yticks=[])
+            ax.imshow(x_train[idx].reshape(28, 28), cmap="gray")
+            ax.set_title(str(y_train[idx].item()))
+        plt.suptitle("MINST training dataset (first 10 images)")
+        plt.show()
+
+        """
+        Model in https://medium.com/p/555007a50c2e (maxPool makes training faster)
+        inputs = tf.keras.Input(shape=(28, 28, 1))
+        x = tf.keras.layers.Conv2D(32, (5, 5), activation=tf.nn.relu)(inputs)
+        x = tf.keras.layers.MaxPool2D((2, 2))(x)
+        x = tf.keras.layers.Conv2D(32, (5, 5), activation=tf.nn.relu)(x)
+        x = tf.keras.layers.MaxPool2D((2, 2))(x)
+        x = tf.keras.layers.Conv2D(64, (3, 3), activation=tf.nn.relu)(x)
+        x = tf.keras.layers.MaxPool2D((2, 2))(x)
+        x = tf.keras.layers.Flatten()(x)
+        x = tf.keras.layers.Dense(120, activation=tf.nn.relu)(x)
+        x = tf.keras.layers.Dense(84, activation=tf.nn.relu)(x)
+        """
+
+        inputs = tf.keras.Input(shape=(28, 28, 1))
+        x = tf.keras.layers.Conv2D(32, (3, 3), activation=tf.nn.relu)(inputs)
+        x = tf.keras.layers.MaxPool2D((2, 2))(x)
+        x = tf.keras.layers.Conv2D(32, (3, 3), activation=tf.nn.relu)(x)
+        x = tf.keras.layers.MaxPool2D((2, 2))(x)
+        x = tf.keras.layers.Conv2D(64, (3, 3), activation=tf.nn.relu)(x)
+        x = tf.keras.layers.MaxPool2D((2, 2))(x)
+        x = tf.keras.layers.Flatten()(x)
+
+        # Keras Tensorflow .Dense is similar as PyTorch nn.Linear if the function's
+        # parameters are,
+        # see https://stackoverflow.com/questions/72803869/are-those-keras-and-pytorch-snippets-equivalent
+        x = tf.keras.layers.Dense(10, activation=None)(x)
+        """
+        The neural net in PyTorch:
+        self.model = nn.Sequential(
+            nn.Conv2d(1, 32, (3, 3)),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, (3, 3)),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, (3, 3)),
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(64 * (28 - 6) * (28 - 6), 10),
+        )
+        """
+
+
+        outputs = tf.keras.layers.Dense(10, activation=tf.nn.softmax)(x)
+        model = tf.keras.Model(inputs, outputs)
+        model.summary()
+
+        opt = tf.keras.optimizers.Adam(learning_rate=0.001)
+
+        model.compile(
+            optimizer=opt, loss="sparse_categorical_crossentropy", metrics=["accuracy"]
+        )
+
+        checkpoint = tf.keras.callbacks.ModelCheckpoint(
+            model_fname,
+            monitor="accuracy",
+            verbose=1,
+            save_best_only=True,
+            save_weights_only=False,
+            mode="auto",
+            save_freq="epoch",
+        )
+        t1_start = perf_counter()
+        model.fit(x_train, y_train, epochs=n_epochs_to_train, verbose=2, callbacks=[checkpoint])
+        t1_end = perf_counter()
+        print(f"Time for training using Keras is {t1_end - t1_start:.2f} seconds.")
+
+    # Prediction
+    model = load_model(model_fname)
+    t1 = perf_counter()
+    test_loss, test_acc = model.evaluate(x_test, y_test)
+    t2 = perf_counter()
+    print(
+        f"Evaluation accuracy using Keras is {test_acc * 100:.2f} and execution time {t2 - t1:.2f} seconds."
+    )
--- torch_image_nn.py
+++ torch_image_nn.py
@@ -1,6 +1,10 @@
-# This code was created by following the YouTube tutorial on building a neural 
-# network with PyTorch, https://www.youtube.com/watch?v=mozBidd58VQ
+# This code was adapted from this tutorial, https://www.youtube.com/watch?v=mozBidd58VQ
+# Compared to the tutorial, the neural network architecture was
+# changed: To speed pu training, the second conv2D layer has only 32 outputs
+# and maxpool layers were introduced
 
+import matplotlib.pyplot as plt
+from time import perf_counter
 import torch
 from PIL import Image
 from torch import nn, save, load
@@ -9,42 +13,54 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 
+
+do_train = True
+n_epochs_to_train = 3
+
 class ImageClassifier(nn.Module):
     def __init__(self):
         super().__init__()
         self.model = nn.Sequential(
-            nn.Conv2d(1, 32, (3,3)),
+            nn.Conv2d(1, 32, (3, 3)),
+            nn.MaxPool2d((2, 2)),
             nn.ReLU(),
-            nn.Conv2d(32, 64, (3,3)),
+            nn.Conv2d(32, 32, (3, 3)),
+            nn.MaxPool2d((2, 2)),
             nn.ReLU(),
-            nn.Conv2d(64, 64, (3,3)),
+            nn.Conv2d(32, 64, (3, 3)),
+            nn.MaxPool2d((2, 2)),
             nn.ReLU(),
             nn.Flatten(),
-            nn.Linear(64 * (28 - 6) * (28 - 6), 10)
+            nn.Linear(64, 10),
         )
-    
 
     def forward(self, x):
         return self.model(x)
-    
+
+    def count_parameters(self):
+        # Thanks to: https://discuss.pytorch.org/t/how-do-i-check-the-number-of-parameters-of-a-model/4325/9
+        return sum(p.numel() for p in self.model.parameters() if p.requires_grad)
+
 
 # Instance the neural network, loss, and optimizer
-compute_type = 'cpu' # 'cuda' if you have GPU, otherwise 'cpu'
-clf = ImageClassifier().to(compute_type) 
+compute_type = "cpu"  # 'cuda' if you have GPU, otherwise 'cpu'
+clf = ImageClassifier().to(compute_type)
 opt = Adam(clf.parameters(), lr=1e-3)
 loss_fn = nn.CrossEntropyLoss()
 
 
-do_train = False
 if __name__ == "__main__":
-    model_file_name = 'model_state.pt'
+    model_file_name = "torch_model.pt"
 
-    # Training flow
+    # Training
     if do_train:
-        train = datasets.MNIST(root="data", download=True, train=True, transform=ToTensor())
+        train = datasets.MNIST(
+            root="data", download=True, train=True, transform=ToTensor()
+        )
         dataset = DataLoader(train, 32)
-        
-        for epoch in range(10):
+        print(f"Traing a {clf.count_parameters()} parameter model: {clf}")
+        for epoch in range(n_epochs_to_train):
+            t1_start = perf_counter()
             for batch in dataset:
                 X, y = batch
                 X, y = X.to(compute_type), y.to(compute_type)
@@ -55,19 +71,27 @@ if __name__ == "__main__":
                 opt.zero_grad()
                 loss.backward()
                 opt.step()
+            t1_end = perf_counter()
+            print(f"Epoch: {epoch} loss is {loss.item()}, computing time was {t1_end - t1_start:.2f} seconds.") 
+            
 
-            print(f"Epoch:{epoch} loss is {loss.item()}")
-        
-        with open(model_file_name, 'wb') as f:
+        with open(model_file_name, "wb") as f:
             save(clf.state_dict(), f)
 
-    # Prediction flow
-    with open(model_file_name, 'rb') as f:
+    # Prediction
+    with open(model_file_name, "rb") as f:
         clf.load_state_dict(load(f))
 
-    img_files = ['img_1.jpg', 'img_2.jpg', 'img_3.jpg']
+    print(f"Predicting with a {clf.model.parameters()} parameter model: {clf}")
+    img_files = ["img_1.jpg", "img_2.jpg", "img_3.jpg"]
 
-    for fname in img_files:
+    fig = plt.figure(figsize=(5, 4))
+    for i, fname in enumerate(img_files):
         img = Image.open(fname)
         img_tensor = ToTensor()(img).unsqueeze(0).to(compute_type)
-        print(f"{fname} is {torch.argmax(clf(img_tensor))}")
+        predicted_number = torch.argmax(clf(img_tensor))
+        ax = fig.add_subplot(1, len(img_files), i + 1, xticks=[], yticks=[])
+        ax.imshow(img, cmap="gray")
+        ax.set_title(str(int(predicted_number)))
+    fig.suptitle("Predicted numbers")
+    plt.show()
